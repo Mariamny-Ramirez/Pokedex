00c71eed06e5f098d131aef8ff66d15908a3997e
mariamny <mariamnydelvalle@gmail.com>
2026-05-02 19:09:42 -0500

    Starting pokedex

---
+++ src/data/pokeneas.js
@@ -0,0 +1,70 @@
+// data/pokeneas.js
+
+const pokeneas = [
+  {
+    id: 1,
+    nombre: "Rayquaza",
+    altura: "7.0 m",
+    habilidad: "Air Lock",
+    imagen: "https://img.pokemondb.net/artwork/large/rayquaza.jpg",
+    frase: "El que no arrisca, no pasa la trocha."
+  },
+  {
+    id: 2,
+    nombre: "Agrron",
+    altura: "2.1 m",
+    habilidad: "Sturdy",
+    imagen: "https://img.pokemondb.net/artwork/large/aggron.jpg",
+    frase: "Ome, la vida es dura pero yo soy más."
+  },
+  {
+    id: 3,
+    nombre: "Charizard",
+    altura: "1.7 m",
+    habilidad: "Blaze",
+    imagen: "https://img.pokemondb.net/artwork/large/charizard.jpg",
+    frase: "Échele la culpa al frío, pero arrímese al fogón."
+  },
+  {
+    id: 4,
+    nombre: "Garchomp",
+    altura: "1.9 m",
+    habilidad: "Sand Veil",
+    imagen: "https://img.pokemondb.net/artwork/large/garchomp.jpg",
+    frase: "El que mucho abarca, poco aprieta, mijo."
+  },
+  {
+    id: 5,
+    nombre: "Infernape",
+    altura: "1.2 m",
+    habilidad: "Blaze",
+    imagen: "https://img.pokemondb.net/artwork/large/infernape.jpg",
+    frase: "Mono que se trepa, llega lejos."
+  },
+  {
+    id: 6,
+    nombre: "Hydreigon",
+    altura: "1.8 m",
+    habilidad: "Levitate",
+    imagen: "https://img.pokemondb.net/artwork/large/hydreigon.jpg",
+    frase: "Tres cabezas piensan más que una, ome."
+  },
+  {
+    id: 7,
+    nombre: "Nidoking",
+    altura: "1.4 m",
+    habilidad: "Poison Point",
+    imagen: "https://img.pokemondb.net/artwork/large/nidoking.jpg",
+    frase: "Póngale la corona al que se la gana."
+  },
+  {
+    id: 8,
+    nombre: "Venusaur",
+    altura: "2.0 m",
+    habilidad: "Overgrow",
+    imagen: "https://img.pokemondb.net/artwork/large/venusaur.jpg",
+    frase: "Raíces paisas, flores pa' el mundo."
+  }
+];
+
+module.exports = pokeneas;
